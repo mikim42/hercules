5678fe56a10f68608c9165bd651b89c15c906e8f
kimminseok <mikim@student.42.us.org>
2018-02-23 23:12:28 -0800

    Nemean Lion: updated

--- nemean_lion/setup_ssh.sh
+++ nemean_lion/setup_ssh.sh
@@ -6,7 +6,7 @@
 #    By: Mingyun Kim <mikim@student.42.us.org>      +#+  +:+       +#+         #
 #    GitHub:  https://github.com/mikim42          +#+#+#+#+#+   +#+            #
 #    Created: 2018/02/23 15:40:45 by Mingyun K         #+#    #+#              #
-#    Updated: 2018/02/23 17:39:37 by Mingyun K        ###   ########.fr        #
+#    Updated: 2018/02/23 23:11:52 by Mingyun K        ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,20 +19,22 @@ su
 cp /etc/network/interfaces /etc/network/interfaces-backup
 
 # append
+# first 3 part of IP address should be equal to the vboxnet0 addr
+# last one should be different from that
+# ex. same.same.same.diff
 echo "auto eth1" >> /etc/network/interfaces
 echo "iface eth1 inet static" >> /etc/network/interfaces
 echo "address 192.168.56.10" >> /etc/network/interfaces
 echo "netmask 255.255.255.0" >> /etc/network/interfaces
 
-# make a backup
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config sshd_config-backup
-
 # change the port number
 # private port number 49152 ~ 65535
-sed -i "s/^Port .*/Port 50100/g" /etc/ssh/sshd_config
+# -i command creates backups
+sed -i "s/^#\?Port .*/Port 50100/g" /etc/ssh/sshd_config
+
+# run ssh server
+ifup eth1
+service ssh restart
 
 # exit su
 exit
-
-# run ssh server
-sudo ifup eth1
